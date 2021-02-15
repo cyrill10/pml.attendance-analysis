898d7125458731184b5c2843bfa81ef106745cd4
cyrill10 <cyrill.bader@gmx.ch>
2021-02-15 16:02:18 +0100

    start data prep

---
+++ analysis/attancanceUtil.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import locale
+
+team_coordinates_df = pd.read_csv('../data/team_coordinates.csv', index_col=0, header=0)
+league_table_df = pd.read_csv('../data/league-table.csv', index_col=0, header=0)
+
+
+def custom_date_parser(x):
+    result = []
+    locale.setlocale(locale.LC_ALL, 'de_DE')
+    for value in x:
+        result.append(datetime.strptime(value, ' %d. %B %Y'))
+    return result
+
+
+def calc_dist(hometeam, awayteam):
+    home = team_coordinates_df.loc[hometeam]
+    away = team_coordinates_df.loc[awayteam]
+    home_array = np.array((home[0], home[1]))
+    away_array = np.array((away[0], away[1]))
+    return np.linalg.norm(home_array - away_array)
+
+
+def determine_season(date: datetime):
+    year = date.year
+    if date.month <= 6:
+        year -= 1
+    return year
+
+
+def calc_points_before_game(season, matchday, team):
+    matchday -= 1
+    if matchday == 0:
+        return 0, 0
+    table = league_table_df.loc[season]
+    home_mask = np.logical_and(table['matchday'] == matchday, table['team'] == team)
+    points_home = table[home_mask]
+    return points_home['points'][season]
