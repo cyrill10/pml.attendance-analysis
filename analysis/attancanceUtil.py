be6814d52ffc9af499350819ef93b2c75646df62
cyrill10 <cyrill.bader@gmx.ch>
2021-03-02 15:20:14 +0100

    Add more features

--- analysis/attancanceUtil.py
+++ analysis/attancanceUtil.py
@@ -59,3 +59,43 @@ def calc_point_average_before_game(season, matchday, team):
     if season == 2011 and team == 'FC Sion':
         points += 36
     return points/matchday
+
+
+def calc_postion_before_game(season, matchday, team):
+    matchday -= 1
+    if matchday == 0:
+        matchday = 1
+    table = league_table_df.loc[season]
+    mask = np.logical_and(table['matchday'] == matchday, table['team'] == team)
+    table_df = table[mask]
+    position = table_df['place'][season]
+    return position
+
+
+def calc_last_seasons_position(season, team):
+    season -= 1
+    table = league_table_df.loc[season]
+    mask = np.logical_and(table['matchday'] == 36, table['team'] == team)
+    table_df = table[mask]
+    if table_df.empty:
+        position = 10
+    else:
+        position = table_df['place'][season]
+    return position
+
+
+def calc_point_average_from_last_five_games(season, matchday, team, attendance_df):
+    if matchday == 1:
+        return 0
+    matchdays = np.arange(matchday-min(5, matchday-1), matchday)
+    home_mask = np.logical_and(attendance_df['hometeam'] == team, attendance_df['season'] == season)
+    away_mask = np.logical_and(attendance_df['awayteam'] == team, attendance_df['season'] == season)
+    home_df = attendance_df[home_mask].copy()
+    away_df = attendance_df[away_mask].copy()
+    home_df[['for', 'against']] = home_df['result'].str.split(":", expand=True)
+    away_df[['against', 'for']] = away_df['result'].str.split(":", expand=True)
+    home_results = home_df[home_df['matchday'].isin(matchdays)]
+    away_results = away_df[away_df['matchday'].isin(matchdays)]
+    results = pd.concat([home_results, away_results], ignore_index=True)
+    results['points'] = np.where(results['for'] > results['against'], 3, np.where(results['for'] == results['against'], 1, 0))
+    return sum(results.loc[:, 'points']) / len(results)
